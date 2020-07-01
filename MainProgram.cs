54cb47b30b3fed2a6b5987db536eee3d1fe23049
Henriks Auzins <henrikauzins@hotmail.com>
2020-07-01 22:03:16 +0100

    fifth commit

--- MainProgram.cs
+++ MainProgram.cs
@@ -2,25 +2,24 @@
 
 public class MainProgram
 {
-	int num1 = 0;
-	int num2 = 2;
-	public static void MainProgram()
-	{
-		// simple program that adds two inputted numbers 
-		Console.WriteLine("this is a C# file");
-
-		Console.WriteLine("type in a number");
-		num1 = Console.ReadLine(Convert.ToInt32());
-
-		Console.WriteLine("type in another number");
-		num2 = Console.ReadLine(Convert.ToInt32());
-
-		GetNumber(num1, num2);
-	}
-
-	public static int GetNumber(int a, int b)
+	
+    static void Main(string[] args)
     {
-		int total = a + b;
-		return total
+        int num1 = 0;
+        int num2 = 0;
+        int total = 0;
+        // simple program that adds two inputted numbers 
+        Console.WriteLine("this is a C# file");
+
+        Console.WriteLine("type in a number");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("type in another number");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        total = num1 + num2;
+
+        Console.WriteLine(total);
+
     }
 }
