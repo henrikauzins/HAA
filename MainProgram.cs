6115e3650a82ea72f844e721523a98bd6019d6a4
Henriks Auzins <henrikauzins@hotmail.com>
2020-07-01 21:24:42 +0100

    C# file commit

---
+++ MainProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class MainProgram
+{
+	int num1 = 0;
+	int num2 = 2;
+	public static void MainProgram()
+	{
+		// simple program that adds two inputted numbers 
+		Console.WriteLine("this is a C# file");
+
+		Console.WriteLine("type in a number");
+		num1 = Console.ReadLine(Convert.ToInt32());
+
+		Console.WriteLine("type in another number");
+		num2 = Console.ReadLine(Convert.ToInt32());
+
+		GetNumber(num1, num2);
+	}
+
+	public static int GetNumber(int a, int b)
+    {
+		int total = a + b;
+		return total
+    }
+}
